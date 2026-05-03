c73d9f23dca9e841acadbbbe601f3a9aa83f8e8a
msmfathih <msmfathih@users.noreply.github.com>
2026-05-03 18:08:24 +0400

    new test cases added

--- tests/purchase.spec.js
+++ tests/purchase.spec.js
@@ -10,7 +10,31 @@ test("Login test", async function({page}) {
 
     await page.locator("//input[@id='login-button']").click()
 
-    //await page.waitForTimeout(1000)
+    await page.locator("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']").click()
+
+    await page.locator("//button[@id='add-to-cart-sauce-labs-fleece-jacket']").click()
+
+    await page.locator("//a[@class='shopping_cart_link']").click()
+
+    await page.locator("//button[@id='checkout']").click()
+
+    await page.getByPlaceholder("First Name").fill("aadil")
+
+    await page.getByPlaceholder("Last Name").fill("ahamed")
+
+    await page.getByPlaceholder("Zip/Postal Code").fill("12123")
+
+    await page.locator("//input[@id='continue']").click()
+
+    await page.locator("//button[@id='finish']").click()
+
+    const verifySuccessMessage=await page.locator("//h2[normalize-space()='Thank you for your order!']").textContent()
+
+    console.log("The message is "+verifySuccessMessage);
+
+    expect(verifySuccessMessage.includes("Thank you for your")).toBeTruthy()
+
+    //await page.waitForTimeout(5000)
 
     await expect(page).toHaveURL(/saucedemo/);
 
