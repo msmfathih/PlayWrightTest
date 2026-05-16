fb1f680a8b99c10ca70f22c7dc9c207fb3761e2e
msmfathih <msmfathih@users.noreply.github.com>
2026-05-16 20:25:18 +0400

    viewport updated

--- tests/loginapplication.spec.js
+++ tests/loginapplication.spec.js
@@ -2,7 +2,7 @@ const {test,expect}=require('@playwright/test')
 const LoginPage=require("../pages/loginpage")
 const { credentials } = require('../testdata/credentials')
 
-test.use({viewport:{width:1501,height:1000}})
+test.use({viewport:{width:1502,height:1000}})
 
 test("Login test", async function({page}) {
 
